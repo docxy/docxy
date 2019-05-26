40596e65a4a5493a8dc790c432347b999ea0510c
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-05-26 18:00:48 +0530

    commands: add serve command

---
+++ commands/serve.js
@@ -0,0 +1,24 @@
+const shell = require("shelljs");
+
+exports.command = "serve [port]";
+
+exports.describe = "Serves the documentation in browser so that you can preview it while working on it.";
+
+exports.builder = (yargs) => {
+  yargs.positional("port", {
+      describe: "port to bind on",
+      type: "number",
+      default: 8000,
+  });
+};
+
+exports.handler = function (argv) {
+  // Move shell runtime inside `.awesome` directory.
+  if (shell.cd(".awesome").code !== 0) {
+    shell.echo("Hint: Run `awesomedocs init` to initialize AwesomeDocs");
+    shell.exit(1);
+  }
+
+  // Start Gatsby development server
+  shell.exec("gatsby develop --port " + argv.port);
+};
