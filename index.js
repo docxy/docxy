e2f65ecbca565258e24ae53da68425200abf3701
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-05-26 18:01:50 +0530

    implement all commands

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const yargs = require("yargs")
 const loadCommamnd = (command) => require("./commands/" + command);
 
 yargs
+    .command(loadCommamnd("init"))
+    .command(loadCommamnd("build"))
+    .command(loadCommamnd("serve"))
     .argv;
