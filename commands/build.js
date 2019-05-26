bff476b22e6036f958533a0836cfe86d9bcd9ac3
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-05-26 18:00:38 +0530

    commands: add build command

---
+++ commands/build.js
@@ -0,0 +1,18 @@
+const shell = require("shelljs");
+
+exports.command = "build";
+
+exports.describe = "Builds the documentation website";
+
+exports.builder = () => {};
+
+exports.handler = function (argv) {
+  // Move shell runtime inside `.awesome` directory.
+  if (shell.cd(".awesome").code !== 0) {
+    shell.echo("Hint: Run `awesomedocs init` to initialize AwesomeDocs");
+    shell.exit(1);
+  }
+
+  // Build the documentation webisite
+  shell.exec("gatsby build");
+};
